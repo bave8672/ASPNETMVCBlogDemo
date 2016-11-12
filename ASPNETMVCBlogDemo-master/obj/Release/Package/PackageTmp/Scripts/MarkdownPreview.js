93c4bda38d51ea99aa80378973020e99bb88821f
Ben Aves <avesbenjamin@gmail.com>
2016-11-12 21:45:53 +1100

    remove /bin and /obj files

--- ASPNETMVCBlogDemo-master/obj/Release/Package/PackageTmp/Scripts/MarkdownPreview.js
+++
@@ -1,9 +0,0 @@
-﻿var markdown = new MarkdownDeep.Markdown();
-var titleInput = $(".titleInput");
-var titleOutput = $(".titleOutput");
-var input = $(".mdInput");
-var output = $(".mdPreview");
-input.keyup(function () {
-    titleOutput.text(titleInput.val());
-    output.html(markdown.Transform(input.val()));
-});
